e19bdcf6594b06725ce8e379b83f1fb400e4af63
user3689robloxprojectstudio <benjamin_sumner@hotmail.com>
2026-08-10 08:22:29 +0100

    Restore index.html, server.js, and v4 build.yml

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not found');
+        } else {
+            res.writeHead(200);
+            res.end(content);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
